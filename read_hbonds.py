a2eaf7cf029972d10195074404ba8c3ee719887d
ejjordan <e.jjordan12@gmail.com>
2018-02-07 11:27:55 -0500

    hbonds calcs now use chains

--- read_hbonds.py
+++ read_hbonds.py
@@ -5,18 +5,34 @@ import json
 
 def read_hbonds(**kwargs):
 
-
 	#---unpack
 	sn = kwargs['sn']
 	work = kwargs['workspace']
 	data=kwargs['upstream']['hbonds']
 	divy=kwargs['calc']['specs']['divy']
-	active=work.meta[sn]['active']
-	name=work.meta[sn]['name']
+
+	#this hack guesses the chain breaks based on residue numbering                                      
+	#if residue[i]-residue[i-1]!=1 then assume a chain break                                            
+	if 'startres_idxs' in data:
+		startres_idxs=data['startres_idxs']
+		alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ' #max number of chains=26                              
+		if len(startres_idxs)>len(alphabet):
+			raise Exception("you can't have more than 26 chains")
+		chain_locs=[[alphabet[i],startres_idxs[i],startres_idxs[i+1]-1] for i in range(len(startres_idxs)-1)]
+		#for last chain residx just needs to be a number                                            
+		chain_locs.append([alphabet[len(startres_idxs)-1],startres_idxs[len(startres_idxs)-1],99999])
+	else: chain_locs=None
+	if 'kcat' in work.meta[sn]: active=work.meta[sn]['kcat']
+	else: active=None
+	if 'name' in work.meta[sn]: name=work.meta[sn]['name']
+	else: name=None
 
 	#---compute
-	bonds=hbonds_info(data,timesteps=True,donor_reslist=None,
-					  acceptor_reslist=None,divy=divy)
+	bonds=hbonds_info(data,timesteps=True,
+					  donor_reslist=None,
+					  acceptor_reslist=None,
+					  chain_locs=chain_locs,
+					  divy=divy)
 
 	#---pack
 	attrs,results = {},{}
@@ -25,7 +41,7 @@ def read_hbonds(**kwargs):
 	return results,attrs
 
 def hbonds_info(data,timesteps=False,donor_restype=None,acceptor_restype=None,
-				donor_reslist=None,acceptor_reslist=None,divy=False):
+				donor_reslist=None,acceptor_reslist=None,divy=False,chain_locs=None):
 	hbonds={}
 	main_donor='N'
 	main_acceptor='O'
@@ -57,12 +73,24 @@ def hbonds_info(data,timesteps=False,donor_restype=None,acceptor_restype=None,
 					acceptor_loc='main'
 				else:
 					acceptor_loc='side'
-				label='{0} {1} {2} {3} {4} {5}'.format(donor_restype,
-													   donor_residx,
-													   donor_loc,
-													   acceptor_restype,
-													   acceptor_residx,
-													   acceptor_loc)
+				if chain_locs:
+					donor_chain,acceptor_chain=get_chains(donor_residx,
+														  acceptor_residx,chain_locs)
+					label='{0} {1} {2} {3} {4} {5} {6} {7}'.format(donor_restype,
+																   donor_residx,
+																   donor_loc,
+																   donor_chain,
+																   acceptor_restype,
+																   acceptor_residx,
+																   acceptor_loc,
+																   acceptor_chain)
+				else:
+					label='{0} {1} {2} {3} {4} {5}'.format(donor_restype,
+														   donor_residx,
+														   donor_loc,
+														   acceptor_restype,
+														   acceptor_residx,
+														   acceptor_loc)
 				if label in divies.keys():
 					if timesteps:
 						time=divies[label]
@@ -74,55 +102,122 @@ def hbonds_info(data,timesteps=False,donor_restype=None,acceptor_restype=None,
 						divies[label]=[ts]
 					else: divies[label]=float(ts)
 			if not divy:
-				label='{0} {1} {2} {3} {4} {5}'.format(donor_restype,
-													   donor_residx,
-													   donor_ha,
-													   acceptor_restype,
-													   acceptor_residx,
-													   acceptor_ha)
+				if chain_locs:
+					donor_chain,acceptor_chain=get_chains(donor_residx,
+														  acceptor_residx,chain_locs)
+					label='{0} {1} {2} {3} {4} {5} {6} {7}'.format(donor_restype,
+																   donor_residx,
+																   donor_ha,
+																   donor_chain,
+																   acceptor_restype,
+																   acceptor_residx,
+																   acceptor_ha,
+																   acceptor_chain)
+
+				else:
+					label='{0} {1} {2} {3} {4} {5}'.format(donor_restype,
+														   donor_residx,
+														   donor_ha,
+														   acceptor_restype,
+														   acceptor_residx,
+														   acceptor_ha)
 				if timesteps:
 					if label in hbonds.keys():
 						time=hbonds[label]['times']
 						time.append(ts)
 						hbonds[label]['times']=time
-					else: hbonds[label]={'donor_restype':donor_restype,
-										 'donor_residx':donor_residx,
-										 'donor_HA':donor_ha,
-										 'acceptor_restype':acceptor_restype,
-										 'acceptor_residx':acceptor_residx,
-										 'acceptor_HA':acceptor_ha,
-										 'times':[ts]}
+					else:
+						if chain_locs:
+							donor_chain,acceptor_chain=get_chains(donor_residx,
+																  acceptor_residx,chain_locs)
+							hbonds[label]={'donor_restype':donor_restype,
+										   'donor_residx':donor_residx,
+										   'donor_HA':donor_ha,
+										   'donor_chain':donor_chain,
+										   'acceptor_restype':acceptor_restype,
+										   'acceptor_residx':acceptor_residx,
+										   'acceptor_HA':acceptor_ha,
+										   'acceptor_chain':acceptor_chain,
+										   'times':[ts]}
+						else:
+							hbonds[label]={'donor_restype':donor_restype,
+										   'donor_residx':donor_residx,
+										   'donor_HA':donor_ha,
+										   'acceptor_restype':acceptor_restype,
+										   'acceptor_residx':acceptor_residx,
+										   'acceptor_HA':acceptor_ha,
+										   'times':[ts]}
 				if not timesteps:
 					if label in hbonds.keys():
 						hbonds[label]['occupancy']+=float(ts)
 					else:
-						hbonds[label]={'donor_restype':donor_restype,
-									   'donor_residx':donor_residx,
-									   'donor_HA':donor_ha,
-									   'acceptor_restype':acceptor_restype,
-									   'acceptor_residx':acceptor_residx,
-									   'acceptor_HA':acceptor_ha,
-									   'occupancy':float(ts)}
+						if chain_locs:
+							donor_chain,acceptor_chain=get_chains(donor_residx,
+																  acceptor_residx,chain_locs)
+							hbonds[label]={'donor_restype':donor_restype,
+										   'donor_residx':donor_residx,
+										   'donor_HA':donor_ha,
+										   'donor_chain':donor_chain,
+										   'acceptor_restype':acceptor_restype,
+										   'acceptor_residx':acceptor_residx,
+										   'acceptor_HA':acceptor_ha,
+										   'acceptor_chain':acceptor_chain,
+										   'occupancy':float(ts)}
+
+						else:
+							hbonds[label]={'donor_restype':donor_restype,
+										   'donor_residx':donor_residx,
+										   'donor_HA':donor_ha,
+										   'acceptor_restype':acceptor_restype,
+										   'acceptor_residx':acceptor_residx,
+										   'acceptor_HA':acceptor_ha,
+										   'occupancy':float(ts)}
 
 	if divy:
 		for key,val in divies.items():
 			infos=key.split(' ')
 			if timesteps:
-				hbonds[key]={'donor_restype':infos[0],
-							'donor_residx':infos[1],
-							'donor_loc':infos[2],
-							'acceptor_restype':infos[3],
-							'acceptor_residx':infos[4],
-							'acceptor_loc':infos[5],
-							'times':val}
+				if chain_locs:
+					donor_chain,acceptor_chain=get_chains(donor_residx,
+														  acceptor_residx,chain_locs)
+					hbonds[key]={'donor_restype':infos[0],
+								 'donor_residx':infos[1],
+								 'donor_loc':infos[2],
+								 'donor_chain':infos[3],
+								 'acceptor_restype':infos[4],
+								 'acceptor_residx':infos[5],
+								 'acceptor_loc':infos[6],
+								 'acceptor_chain':infos[7],
+								 'times':val}
+				else:
+					hbonds[key]={'donor_restype':infos[0],
+								 'donor_residx':infos[1],
+								 'donor_loc':infos[2],
+								 'acceptor_restype':infos[3],
+								 'acceptor_residx':infos[4],
+								 'acceptor_loc':infos[5],
+								 'times':val}
 			if not timesteps:
-				hbonds[key]={'donor_restype':infos[0],
-							 'donor_residx':infos[1],
-							 'donor_loc':infos[2],
-							 'acceptor_restype':infos[3],
-							 'acceptor_residx':infos[4],
-							 'acceptor_loc':infos[5],
-							 'occupancy':float(val)}
+				if chain_locs:
+					donor_chain,acceptor_chain=get_chains(donor_residx,
+														  acceptor_residx,chain_locs)
+					hbonds[key]={'donor_restype':infos[0],
+								 'donor_residx':infos[1],
+								 'donor_loc':infos[2],
+								 'donor_chain':infos[3],
+								 'acceptor_restype':infos[4],
+								 'acceptor_residx':infos[5],
+								 'acceptor_loc':infos[6],
+								 'acceptor_chain':infos[7],
+								 'occupancy':float(val)}
+				else:
+					hbonds[key]={'donor_restype':infos[0],
+								 'donor_residx':infos[1],
+								 'donor_loc':infos[2],
+								 'acceptor_restype':infos[3],
+								 'acceptor_residx':infos[4],
+								 'acceptor_loc':infos[5],
+								 'occupancy':float(val)}
 	for hb in hbonds.keys():
 		if timesteps:
 			hbonds[hb]['occupancy']=float('{:0.5f}'.format(len(
@@ -130,3 +225,10 @@ def hbonds_info(data,timesteps=False,donor_restype=None,acceptor_restype=None,
 		else:
 			hbonds[hb]['occupancy']=float('{:0.5f}'.format(hbonds[hb]['occupancy']))
 	return hbonds
+
+def get_chains(donor_residx,acceptor_residx,chain_locs):
+	donor_chain=[chain[0] for chain in chain_locs if
+				 donor_residx>=chain[1] and donor_residx<=chain[2]]
+	acceptor_chain=[chain[0] for chain in chain_locs if
+					acceptor_residx>=chain[1] and acceptor_residx<=chain[2]]
+	return donor_chain[0], acceptor_chain[0]
